d892cbd2d1e9ddf36b0332e9e6163e61ace8b4ac
ChrisStyles99 <chrisrdz99@hotmail.com>
2021-01-09 16:51:12 -0600

    Added login function to front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,34 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import { Home } from './components/Home';
 import { Chat } from './components/Chat';
 import io from 'socket.io-client';
 import { Navbar } from './components/Navbar';
 import { Register } from './components/Register';
 import { Login } from './components/Login';
-// import { Component } from 'react';
+import {useSelector} from 'react-redux';
 
 const URL = 'http://localhost:3001';
 
 const socket = io.connect(URL);
 
-// const ProtectedRoute = ({component: Component, ...rest}) => {
-//   return (
-//     <Route {...rest} render={
-//       (props) => {
-//         if(token === null) {
-          
-//         }
-//       }
-//     }>
-//     </Route>
-//   )
-// }
+const ProtectedRoute = ({component: Component, ...rest}) => {
+
+  const isLoggedIn = useSelector(state => state.isLoggedIn);
+
+  return (
+    <Route {...rest} render={
+      (props) => {
+        if(!isLoggedIn) {
+          return <Redirect to={{pathname: '/login'}} />
+        } else {
+          return <Component {...props} />
+        }
+      }
+    }>
+    </Route>
+  )
+}
 
 function App() {
   return (
@@ -31,10 +36,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path="/" component={(props) => <Home {...props} socket={socket} />} />
-          <Route exact path="/chat" component={(props) => <Chat {...props} socket={socket} />} />
-          <Route exact path="/register" component={(props) => <Register {...props} />} />
-          <Route exact path="/login" component={(props) => <Login {...props} />} />
+          <ProtectedRoute exact path="/" component={(props) => <Home {...props} socket={socket} />} />
+          <ProtectedRoute exact path="/chat" component={(props) => <Chat {...props} socket={socket} />} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </Router>
     </div>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useDispatch, useSelector} from 'react-redux';
+import { login } from '../actions';
 
-export const Login = () => {
+export const Login = (props) => {
+
+  const dispatch = useDispatch();
+  const error = useSelector(state => state.loginError)
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    
+    if(email === '' || password === '') {
+      return
+    }
+
+    const user = {
+      email, password
+    }
+
+    await dispatch(login(user));
+
+    if(error !== null) return 
+
+    props.history.push('/');
+  }
+
   return (
     <div className="h-screen container m-auto flex justify-center items-center">
-      <form className="flex flex-col bg-gray-200 p-10 shadow-lg rounded-3xl w-1/3">
+      <form className="flex flex-col bg-gray-200 p-10 shadow-lg rounded-3xl w-1/3" onSubmit={handleSubmit}>
+        <p className="text-center text-red-400">{error}</p>
         <h1 className="text-5xl mb-2 text-center">Login</h1>
         <label className="text-lg my-2" >Email:</label>
-        <input type="email" className="border-gray-600 border-2 rounded p-1 outline-none"/>
+        <input type="email" className="border-gray-600 border-2 rounded p-1 outline-none" value={email} onChange={e => setEmail(e.target.value)}/>
         <label className="text-lg my-2" >Password:</label>
-        <input type="password" className="border-gray-600 border-2 rounded p-1 outline-none"/>
+        <input type="password" className="border-gray-600 border-2 rounded p-1 outline-none" value={password} onChange={e => setPassword(e.target.value)}/>
         <button type="submit" className="bg-gray-800 hover:bg-gray-600 transition my-2 rounded py-2 text-white outline-none">Login</button>
       </form>
     </div>
